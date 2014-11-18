706a73b3c82a66cd871301237a24942732955da2
rajaditya <rajaditya.mukherjee@gmail.com>
2014-11-17 21:32:35 -0500

    Forgot to add the main widget function

---
+++ TubicSplines.cpp
@@ -0,0 +1,14 @@
+#include "TubicSplines.h"
+
+Tubic_Splines::Tubic_Splines(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	ui_custom.setUpUi(this,ui);
+
+}
+
+Tubic_Splines::~Tubic_Splines()
+{
+
+}
